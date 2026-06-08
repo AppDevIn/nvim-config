494ec175c4bc97a9da00b25d7fcba704f179c0d6
Jeya <e1356515@u.nus.edu>
2026-06-08 08:04:05 +0800

    Add friendly snippets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,6 +2,7 @@ return {
   {
     "saghen/blink.cmp",
     version = "*",
+    dependencies = { "rafamadriz/friendly-snippets" },
     opts = {
       keymap = {
         preset = "default",
@@ -11,6 +12,7 @@ return {
       appearance = { nerd_font_variant = "mono" },
       completion = { documentation = { auto_show = true } },
       sources = { default = { "lsp", "path", "snippets", "buffer" } },
+      snippets = { preset = "default" },
     },
   },
 }
