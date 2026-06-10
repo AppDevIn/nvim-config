4c3a72d2b37f0e97f6b75e3c1e4d3fa622b28589
Jeya <e1356515@u.nus.edu>
2026-06-10 13:06:02 +0800

    Use tab for auto complete instead

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,7 +6,7 @@ return {
     opts = {
       keymap = {
         preset = "default",
-        ["<S-Tab>"] = { "accept", "fallback" },
+        ["<Tab>"] = { "accept", "fallback" },
         ["<CR>"] = { "fallback" },
       },
       appearance = { nerd_font_variant = "mono" },
