aa78d53aa3bfc736132a898d5ced163a8c3fece4
Jeya <jeyavishnu.subramaniam@ventitechnologies.com>
2026-06-19 12:01:50 +0800

    Add relative number and improve search

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.opt.shortmess:append("S")
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
