e8d19f4707f53274f5ddab7eceb696dfd10dff15
Jeya <jeyavishnu.subramaniam@ventitechnologies.com>
2026-06-19 11:38:10 +0800

    Add lines, words highlight and searchcount

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,17 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = "dracula",
+        theme = "auto",
+        section_separators = { left = "", right = "" },
+        component_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { "searchcount", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
     })
   end,
