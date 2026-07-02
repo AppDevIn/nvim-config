62afacee31754a381745262cd109f0cc06a14d66
Jeya <e1356515@u.nus.edu>
2026-07-02 19:37:17 +0800

    Change formatter

--- lua/plugins/stevearc.lua
+++ lua/plugins/stevearc.lua
@@ -1,7 +1,12 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    formatters_by_ft = { lua = { "stylua" } },
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettier" },
+      html = { "prettier" },
+      css = { "prettier" },
+    },
     format_on_save = { lsp_fallback = true },
   },
 }
